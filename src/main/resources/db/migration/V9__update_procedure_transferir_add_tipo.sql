54ce972ce3ebcabb16f666d304c06b8671a47119
zeotoni <zeotoni666@gmail.com>
2026-09-20 21:25:42 -0300

    ✨ feat: updates transferir procedure to include tipo column via migration V9

---
+++ src/main/resources/db/migration/V9__update_procedure_transferir_add_tipo.sql
@@ -0,0 +1,33 @@
+CREATE OR REPLACE PROCEDURE transferir (valor IN NUMBER, conta_origemID IN NUMBER, conta_destinoID IN NUMBER)
+IS
+    v_saldo_origem NUMBER;
+    v_saldo_destino NUMBER;
+BEGIN
+    IF conta_origemID = conta_destinoID THEN
+       RAISE_APPLICATION_ERROR(-20001, 'Os números precisam ser de contas diferentes!');
+END IF;
+
+    IF valor <= 0 THEN
+       RAISE_APPLICATION_ERROR(-20002, 'O valor de transferência precisa ser maior que 0!');
+END IF;
+
+
+SELECT saldo INTO  v_saldo_origem FROM contas WHERE contas.id = conta_origemID FOR UPDATE;
+SELECT saldo INTO  v_saldo_destino FROM contas WHERE contas.id = conta_destinoID;
+
+IF v_saldo_origem >= valor THEN
+UPDATE contas SET saldo = saldo - valor WHERE id = conta_origemID;
+UPDATE contas SET saldo = saldo + valor WHERE id = conta_destinoID;
+INSERT INTO transacoes (valor, conta_origem_id, conta_destino_id, tipo)
+VALUES (valor, conta_origemID, conta_destinoID, 'TRANSFERENCIA');
+ELSE
+        RAISE_APPLICATION_ERROR(-20003, 'O saldo disponível é insuficiente!');
+END IF;
+
+EXCEPTION
+      WHEN NO_DATA_FOUND THEN
+        RAISE_APPLICATION_ERROR(-20004, 'Conta não encontrada');
+WHEN OTHERS THEN
+        RAISE;
+END;
+/
